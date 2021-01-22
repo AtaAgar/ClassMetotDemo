845352b6bfb440093ec37a4af49dcb2aae209190
edanur çetin <edanurcetin16@hotmail.com>
2021-01-22 21:36:07 +0300

    Add project files.

---
+++ MusteriMenager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class MusteriMenager
+    {
+        public void Ekle( Musteri musteri )
+        {
+            Console.WriteLine("Sayın " + musteri.Adi + " " + musteri.SoyAdi + ", Kaydınız Eklenmiştir. ");
+        }
+
+        public void Sil( Musteri musteri )
+        {
+            Console.WriteLine("Sayın " + musteri.Adi + " " + musteri.SoyAdi + ", Kaydınız Silinmiştir. ");
+        }
+
+        public void Listele( Musteri musteri )
+        {
+            Console.WriteLine("Sayın " + musteri.Adi + " " + musteri.SoyAdi + ", müşteri bilgileriniz aşağıdaki gibidir.");
+            Console.WriteLine("Adı Soyadı: " + musteri.Adi + " " + musteri.SoyAdi );
+            Console.WriteLine("Telefon numaranız: " + musteri.Telefon);
+            Console.WriteLine("ID Numaranız: " + musteri.Id );
+
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+
+            musteri1.Id = 1;
+            musteri1.Adi = "Edanur ";
+            musteri1.SoyAdi = "Çetin";
+            musteri1.Telefon = "05998887766";
+
+            Musteri musteri2 = new Musteri();
+
+            musteri2.Id = 2;
+            musteri2.Adi = "Rabia";
+            musteri2.SoyAdi = "Çetin";
+            musteri2.Telefon = "05443332211";
+
+            MusteriMenager musteri = new MusteriMenager();
+            musteri.Ekle(musteri1);
+            musteri.Ekle(musteri2);
+            musteri.Sil(musteri2);
+            musteri.Listele(musteri1);
+        }
+    }
+}
